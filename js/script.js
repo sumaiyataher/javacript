2c758017f5727da86e0d671d050d5aac9451c34a
Sumaiya Taher <110553442+sumaiyataher@users.noreply.github.com>
2024-03-03 19:15:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+//js
+
+// text change
+function textChange(){
+          document.getElementById('t_c').innerHTML = "hello";
+}
+
+
+
+
+
+//jq
